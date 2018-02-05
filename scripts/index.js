15e68e078ddf886eff0718859db2ba9bbad56789
melmanek <glanc.rafal@gmail.com>
2018-02-05 05:09:24 +0100

    simplif

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,44 @@
+class Sprite {
+  constructor(file, array) {
+    this.array = array
+    this.image = new Image();
+    this.image.onload = e => {
 
-const grass = 'assets/tileset.png'
-const game = new Game()
-game.start(320,460)
+      this.draw( )
+    };
+    this.image.src = file;
+  }
+  draw(){
+    const [sx,sy,sw,sh,x,y,w,h] = this.array
+    game.c.drawImage(this.image, sx,sy,sw,sh,x,y,w,h)
+  }
 
-const sprite = new Sprite( grass )
+}
+
+class Tilemap {
+  constructor() {
+    this.ts = 32
+
+  }
+}
+
+class Game extends Tilemap {
+  constructor() {
+    super()
+    this.canvas = document.createElement("canvas");
+    this.c = this.canvas.getContext("2d");
+  }
+  start(width, height) {
+    this.canvas.width = width;
+    this.canvas.height = height;
+    document.body.appendChild(this.canvas);
+  }
+
+}
+const im = `assets/tileset.png`
+const game = new Game();
+
+game.start(320, 460);
+const grass = [ 0, 0, 32, 32,0,0, 32,32,32]
+game.grass = new Sprite(im, grass )
+console.log(game);
